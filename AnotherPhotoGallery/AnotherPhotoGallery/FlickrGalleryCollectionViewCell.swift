e6c766ebecf34826b4e03fa031653722a8d3e179
Tomasz Gontarz <tomasz.t.gontarz@gmail.com>
2015-08-21 12:00:56 +0200

    Create custom collection view cell with simple image view.

---
+++ AnotherPhotoGallery/AnotherPhotoGallery/FlickrGalleryCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrGalleryCollectionViewCell.swift
+//  AnotherPhotoGallery
+//
+//  Created by Tomasz Gontarz on 21/08/15.
+//  Copyright © 2015 Tomasz Gontarz. All rights reserved.
+//
+
+import UIKit
+
+class FlickrGalleryCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photoView: UIImageView!
+}
